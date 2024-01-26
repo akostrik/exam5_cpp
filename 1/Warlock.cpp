ddff4398cae90f37a6d3c02b33dc23324d71cbc8
ann <internet124816@gmail.com>
2024-01-26 23:30:47 +0100

    1 ok

--- 1/Warlock.cpp
+++ 1/Warlock.cpp
@@ -1,2 +1,37 @@
 # include "Warlock.hpp"
 
+Warlock::Warlock(std::string const &name, std::string const &title) {
+  this->name  = name;
+  this->title = title;
+  std::cout << this->name << ": This looks like another boring day." << std::endl;
+};
+
+Warlock::Warlock (const Warlock& obj) {
+  *this = obj;
+};
+
+Warlock::~Warlock() {
+  std::cout << this->name << ": My job here is done!" << std::endl;
+};
+
+Warlock& Warlock::operator = (const Warlock& obj) {
+  this->name  = obj.getName();
+  this->title = obj.getTitle();
+  return *this;
+};
+
+std::string const &Warlock::getName() const {
+  return this->name;
+};
+
+std::string const &Warlock::getTitle() const {
+  return this->title;
+};
+
+void Warlock::Warlock::introduce() const {
+  std::cout << this->name << ": I am " << this->name << ", " << this->title << "!" << std::endl;
+};
+
+void Warlock::setTitle (std::string const &title) {
+  this->title = title;
+}
--- 1/Warlock.hpp
+++ 1/Warlock.hpp
@@ -1,18 +1,27 @@
-#ifndef WARLOCK_HPP                                   ///
+#ifndef WARLOCK_HPP                                                                        ///
 # define WARLOCK_HPP
 
-# include <string>                                    ///
+# include <string>                                                                         ///
+# include <iostream>
 
 class Warlock {
 
-private:
-  std::string  name;
-  std::string  title;
+  private:
+  std::string       name;
+  std::string       title;
 
-               Warlock    ();
-               ~Warlock   ();
-               Warlock    (const Warlock& obj);      /// const
-  Warlock&     operator = (const Warlock& obj);
-}
+                    Warlock    ();
+                    Warlock    (const Warlock& obj);                                       /// const
+  Warlock&          operator = (const Warlock& obj);
+
+  public:
+                    Warlock    (const std::string &name, const std::string &title);        /// const, &
+                    ~Warlock   ();
+
+  const std::string &getName   ()                                                   const; /// const const
+  const std::string &getTitle  ()                                                   const;
+  void              introduce  ()                                                   const; /// const
+  void              setTitle   (const std::string &title);
+};
 
 #endif
