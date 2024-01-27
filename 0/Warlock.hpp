69f430a97b0e7d7a6d519d496ebe805f71c98ccb
ann <internet124816@gmail.com>
2024-01-27 23:21:07 +0100

    0 works verified

--- 0/Warlock.hpp
+++ 0/Warlock.hpp
@@ -4,18 +4,17 @@
 class Warlock {
 
   private:
-  std::string       name;
-  std::string       title;
-
-                    Warlock    ();
-                    Warlock    (const Warlock& obj);                                       /// all args const
-  Warlock&          operator = (const Warlock& obj);
+         std::string name;
+         std::string title;
+                     Warlock    ();
+                     Warlock    (const Warlock& obj);                                     /// all args const
+         Warlock&    operator = (const Warlock& obj);
 
   public:
-                    Warlock    (const std::string name, const std::string title);          // & ?
-                    ~Warlock   ();
-  const std::string &getName   ()                                                   const; /// const const
-  const std::string &getTitle  ()                                                   const;
-  void              introduce  ()                                                   const; /// const
-  void              setTitle   (const std::string &title);
+                     Warlock    (const std::string name, const std::string title);
+                     ~Warlock   ();
+  const std::string& getName    ()                                                 const; /// const const
+  const std::string& getTitle   ()                                                 const;
+        void         introduce  ()                                                 const;
+        void         setTitle   (const std::string &title);
 };
